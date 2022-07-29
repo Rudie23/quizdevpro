409b95f52a588e75a1ab6d362447947a953de2b9
Diego <ruandiego100@gmail.com>
2022-07-28 23:06:51 -0300

    Página classificacao.html não terminada

--- quiz/main/admin.py
+++ quiz/main/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from quiz.main.models import Pergunta
+from quiz.main.models import Pergunta, Aluno, Resposta
 
 
 @admin.register(Pergunta)
 class AdminPergunta(admin.ModelAdmin):
     # Lembre-se: em alternativas use um dicionário JSON, EX - {"array":["function", "def", "if", "pass"]}
     list_display = ('id', 'enunciado', 'disponivel')
+
+
+@admin.register(Aluno)
+class AdminAluno(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'criado_em')
+
+
+@admin.register(Resposta)
+class AdminResposta(admin.ModelAdmin):
+    list_display = ('respondida_em', 'aluno', 'pergunta', 'pontos')
--- quiz/main/models.py
+++ quiz/main/models.py
@@ -5,6 +5,12 @@ from django.db import models
 
 # mvt (model view template)
 
+class Aluno(models.Model):
+    nome = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+
 class Pergunta(models.Model):
     enunciado = models.TextField()
     alternativas = models.JSONField()  # modelo permite ter uma hierarquia que permite colocar várias opções
@@ -19,3 +25,16 @@ class Pergunta(models.Model):
 
     def __str__(self):
         return self.enunciado
+
+
+class Resposta(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    pontos = models.IntegerField()
+    respondida_em = models.DateTimeField(auto_now_add=True)
+
+    # Quero colocar que o usuario possa responder, de forma correta, APENAS UMA PERGUNTA
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['aluno', 'pergunta'], name='resposta_unica')
+        ]
